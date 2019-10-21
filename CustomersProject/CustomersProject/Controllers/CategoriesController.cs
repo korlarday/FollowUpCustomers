9eef73e79f658def53fbf1623158b3800ed561a0
Busari Rilwan <korlarday47@yahoo.com>
2019-10-21 08:10:03 +0100

    Added all functionalities, remaining search functionality

---
+++ CustomersProject/CustomersProject/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CustomersProject.Controllers
+{
+    [Authorize]
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        public ActionResult Pending()
+        {
+            return View();
+        }
+
+        public ActionResult Registered()
+        {
+            return View();
+        }
+    }
+}
